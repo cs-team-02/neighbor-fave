8bb77b0e0b2f865dd0f6bf62324df80ac07d70ea
kwame gumbs <kwame.gumbs@gmail.com>
2022-03-17 14:40:26 -0400

    heroku

--- server/db/models/User.js
+++ server/db/models/User.js
@@ -2,7 +2,6 @@ const Sequelize = require('sequelize')
 const db = require('../db')
 const jwt = require('jsonwebtoken')
 const bcrypt = require('bcrypt');
-const axios = require('axios');
 
 const SALT_ROUNDS = 5;
 
