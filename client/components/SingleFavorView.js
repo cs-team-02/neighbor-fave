3b0397229bf669231cb3d906d14fbb547015bb13
Tedi Lowney <tmlowney1@gmail.com>
2022-03-19 16:27:53 -0400

    User can click button to Resolve or Reopen a favor, toggling status of favor from Open to Closed, with useState hook added to SingleFavorView component

--- client/components/SingleFavorView.js
+++ client/components/SingleFavorView.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 
 const dummyFavor = {
@@ -6,7 +6,7 @@ const dummyFavor = {
   name: "cup of flour",
   description:
     "hello neighbors! I need a cup of flour. It must be gluten free and it needs to happen before noon today. Urgent! It's for a competitive bake-off",
-  status: "Open",
+  status: "Closed",
   author: "Janet",
   lat: 51.615,
   long: -0.09,
@@ -31,10 +31,32 @@ const SingleFavor = () => {
   // then we can use the singleFavor on state to populate this view
   // with all details of the favor (name, description, status, author, pending bids)
   // const id = this.props.match.params.id;
+
+  const [status, setStatus] = useState("");
+
+  useEffect(() => {
+    setStatus(dummyFavor.status);
+  }, []);
+
   return (
     <div>
       <h1>Favor: {dummyFavor.name}</h1>
-      <h2>Status: {dummyFavor.status}</h2>
+      <span> Status: {status} </span>
+      <button
+        onClick={() => {
+          if (status === "Open") {
+            console.log("status is open");
+            setStatus("Closed");
+          } else {
+            console.log("status is closed");
+            setStatus("Open");
+          }
+        }}
+      >
+        {" "}
+        {status === "Open" ? "Resolve" : "Reopen"}{" "}
+      </button>
+
       <h2>Description: {dummyFavor.description}</h2>
       <h2>Author: {dummyFavor.author}</h2>
       <h2>{dummyFavor.bids.length} Pending bids</h2>
@@ -42,6 +64,7 @@ const SingleFavor = () => {
       <button>Offer help with {dummyFavor.name}</button>
       <br />
       <br />
+
       <Link to="/mapView">
         <button>Back to map view</button>
       </Link>
