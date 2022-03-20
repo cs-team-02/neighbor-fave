4f7d94c82585d047ab444849ea54cfc5c5500bd3
Sulaiman Marey <adammarey51@gmail.com>
2022-03-20 13:27:21 -0700

    added favor model

---
+++ server/db/models/Favor.js
@@ -0,0 +1,35 @@
+const { STRING } = require('sequelize');
+const Sequelize = require('sequelize');
+const db = require('../db');
+
+const Favor = db.define('favor', {
+  title: {
+    type: Sequelize.STRING,
+    unique: true,
+    allowNull: false,
+    validate: {
+      notEmpty: true,
+    },
+  },
+  ImageURL: {
+    type: Sequelize.STRING,
+    allowNull: false,
+    defaultValue: '',
+  },
+  description: {
+    type: Sequelize.STRING,
+    allowNull: false,
+  },
+  status: {
+    type: Sequelize.BOOLEAN,
+    defaultValue: false,
+  },
+  dateCreated: {
+    type: Sequelize.DATEONLY,
+    validate: {
+      notEmpty: true,
+    },
+  },
+});
+
+module.exports = Favor;
