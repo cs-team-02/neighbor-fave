dd240eb2eadfa461c5775701c482ae7c1178ca1b
Tedi Lowney <tmlowney1@gmail.com>
2022-03-22 19:27:18 -0400

    Fixed Express Route to GET /api/users/:userId - now attaches the users bids and favors to the user instance object before returning it

--- server/api/users.js
+++ server/api/users.js
@@ -1,6 +1,6 @@
 const router = require("express").Router();
 const {
-  models: { User },
+  models: { User, Favor, Bid },
 } = require("../db");
 module.exports = router;
 
@@ -19,13 +19,31 @@ router.get("/", async (req, res, next) => {
   }
 });
 
-// GET /api/users/:userId    --- get one user, and attach their favors and bids
+// GET /api/users/:userId    --- gets one user, and attach their favors and bids
 router.get("/:userId", async (req, res, next) => {
-  const user = await User.findByPk(req.params.userId, {
-    include: [{ model: Favor }, { model: Bid }],
-  });
+  try {
+    let user = await User.findByPk(req.params.userId);
+    const bids = await Bid.findAll({
+      where: { volunteerId: user.id },
+    });
 
-  // if eager loading can't include these models, then instead query DB for
+    const favors = await Favor.findAll({
+      where: { authorId: user.id },
+    });
+
+    user.dataValues.favors = favors;
+    user.dataValues.bids = bids;
+
+    res.send(user);
+  } catch (error) {
+    console.log("error retrieving user from DB", error);
+  }
+
+  // const user = await User.findByPk(req.params.userId, {
+  //   include: [{ model: Favor }, { model: Bid }],
+  // });
+  // if eager loading can't include models Favor and Bid (b/c of direction of relationship)
+  // then instead query DB for
   // all favors where authorId is this userId,
   // and all bids where volunteerId is this userId
   // get both of these as arrays and attach them to the retrieved user instance
