538d130dab582e946fe241fb3b7c1c203fbc067b
kwame gumbs <kwame.gumbs@gmail.com>
2022-03-22 16:42:21 -0400

    pwa setup

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,6 @@
+const WorkboxPlugin = require('workbox-webpack-plugin');
 module.exports = {
+  mode: 'development',
   entry: [
     './client/index.js'
   ],
@@ -20,5 +22,32 @@ module.exports = {
         }
       }
     ]
-  }
+  },
+  plugins: [
+    // Other plugins...
+
+    new WorkboxPlugin.GenerateSW({
+      // Do not precache images
+      exclude: [/\.(?:png|jpg|jpeg|svg)$/],
+
+      // Define runtime caching rules.
+      runtimeCaching: [{
+        // Match any request that ends with .png, .jpg, .jpeg or .svg.
+        urlPattern: /\.(?:png|jpg|jpeg|svg)$/,
+
+        // Apply a cache-first strategy.
+        handler: 'CacheFirst',
+
+        options: {
+          // Use a custom cache name.
+          cacheName: 'images',
+
+          // Only cache 10 images.
+          expiration: {
+            maxEntries: 10,
+          },
+        },
+      }],
+    })
+  ]
 }
