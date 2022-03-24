1f7a14331d2fce6e53b3f2b852dddf3c09e652a2
Konstantin Balakirev <kbalakirev@gmail.com>
2022-03-24 16:34:58 -0500

    changes evening 3/24

--- client/store/index.js
+++ client/store/index.js
@@ -1,12 +1,12 @@
-import { createStore, combineReducers, applyMiddleware } from "redux";
-import { createLogger } from "redux-logger";
-import thunkMiddleware from "redux-thunk";
-import { composeWithDevTools } from "redux-devtools-extension";
-import auth from "./auth";
-import favors from "./favors";
-import singleFavorReducer from "./SingleFavor";
-import users from "./usersReducer";
-import singleUserReducer from "./singleUserReducer";
+import { createStore, combineReducers, applyMiddleware } from 'redux';
+import { createLogger } from 'redux-logger';
+import thunkMiddleware from 'redux-thunk';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import auth from './auth';
+import favors from './favors';
+import singleFavorReducer from './SingleFavor';
+import users from './usersReducer';
+import singleUserReducer from './singleUserReducer';
 
 const reducer = combineReducers({
   auth,
@@ -21,4 +21,4 @@ const middleware = composeWithDevTools(
 const store = createStore(reducer, middleware);
 
 export default store;
-export * from "./auth";
+export * from './auth';
