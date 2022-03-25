d8be6d5346340a0458aa8c65a31561be88504fdb
Tedi Lowney <tmlowney1@gmail.com>
2022-03-25 13:28:01 -0400

    Now a volunteer can also see the bid, ONLY if they created it. Now chat container for any given bid will be available exclusively to the author of the favor, and the bidder.

--- client/components/Bid.js
+++ client/components/Bid.js
@@ -3,12 +3,20 @@ import { useDispatch } from "react-redux";
 import { acceptBid } from "../store/favors";
 import { fetchSingleFavor } from "../store/SingleFavor";
 import { Link } from "react-router-dom";
+import useAuth from "./utils/useAuthHook";
 // import css from "./Bidstyle.css";
 
 const Bid = (props) => {
-  const { bid } = props;
+  const { bid, favor } = props;
+
   const dispatch = useDispatch();
   const [showChat, setShowChat] = useState(false);
+  const CurrentUser = useAuth();
+  const canViewChat =
+    CurrentUser.id === bid.volunteer.id || CurrentUser.id === favor.author.id;
+
+  // CurrentUser.id === bid.volunteer.id || favor.author.id
+  // CurrentUser === bid.volunteer || favor.author
 
   const handleAcceptBid = async () => {
     await dispatch(acceptBid(bid.id, bid.favorId));
@@ -21,12 +29,10 @@ const Bid = (props) => {
 
   return (
     <div className="bid-container">
-        <span id="bidder-name">from 
-          <Link to={`/users/${bid.volunteer.id}`}>
-            {bid.volunteer.name}
-          </Link>
-          :
-        </span>
+      <span id="bidder-name">
+        from
+        <Link to={`/users/${bid.volunteer.id}`}>{bid.volunteer.name}</Link>:
+      </span>
       <div>{bid.description}</div>
 
       <div>
@@ -35,12 +41,24 @@ const Bid = (props) => {
         <button onClick={handleAcceptBid}>Accept offer</button>
         <br />
       </div>
-      <div id="chat-div">
-        <button onClick={toggleShowChat}>
-          {!showChat ? `Show Chat with ${bid.volunteer.name}` : "Hide Chat"}
-        </button>
-        {showChat ? <div id="chat-box">Chat box here</div> : <div></div>}
-      </div>
+      {canViewChat ? (
+        <div id="chat-div">
+          <button onClick={toggleShowChat}>
+            {!showChat ? `Show Chat ` : "Hide Chat"}
+          </button>
+          {showChat ? (
+            <div id="chat-box">
+              Chat box here: current user is {CurrentUser.id} and favor author
+              is {favor.author.id} and bid volunteer is {bid.volunteer.id}
+            </div>
+          ) : (
+            <div></div>
+          )}
+        </div>
+      ) : (
+        <div>Not your chat</div>
+      )}
+
       <br />
     </div>
   );
