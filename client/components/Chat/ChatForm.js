78e5ea279ca685c616801fd0813b020f5f4a9c32
Sulaiman Marey <adammarey51@gmail.com>
2022-03-28 09:41:28 -0700

    render chatform

--- client/components/Chat/ChatForm.js
+++ client/components/Chat/ChatForm.js
@@ -9,7 +9,38 @@ function ChatForm() {
   const [room, setRoom] = useState('');
   const [showChat, setShowChat] = useState(false);
 
-  return <div></div>;
+  const joinRoom = () => {
+    if (username !== '' && room !== '') {
+      socket.emit('join_room', room);
+      setShowChat(true);
+    }
+  };
+  return (
+    <div className="App">
+      {!showChat ? (
+        <div className="joinChatContainer">
+          <h3>Let's chat!</h3>
+          <input
+            type="text"
+            placeholder="Username..."
+            onChange={(event) => {
+              setUsername(event.target.value);
+            }}
+          />
+          <input
+            type="text"
+            placeholder="Room ID..."
+            onChange={(event) => {
+              setRoom(event.target.value);
+            }}
+          />
+          <button onClick={joinRoom}>Join A Room</button>
+        </div>
+      ) : (
+        <Chat socket={socket} username={username} room={room} />
+      )}
+    </div>
+  );
 }
 
 export default ChatForm;
