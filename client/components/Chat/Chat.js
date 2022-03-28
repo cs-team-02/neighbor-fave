2b0c37097320dba72ac5e455cafb5665e530de8f
Sulaiman Marey <adammarey51@gmail.com>
2022-03-28 09:44:44 -0700

    add sendMessage function

--- client/components/Chat/Chat.js
+++ client/components/Chat/Chat.js
@@ -1,9 +1,27 @@
 import React, { useEffect, useState } from 'react';
+import ScrollToBottom from 'react-scroll-to-bottom';
 
 function Chat({ socket, username, room }) {
   const [currentMessage, setCurrentMessage] = useState('');
   const [messageList, setMessageList] = useState([]);
 
+  const sendMessage = async () => {
+    if (currentMessage !== '') {
+      const messageData = {
+        room: room,
+        author: username,
+        message: currentMessage,
+        time:
+          new Date(Date.now()).getHours() +
+          ':' +
+          new Date(Date.now()).getMinutes(),
+      };
+
+      await socket.emit('send_message', messageData);
+      setMessageList((list) => [...list, messageData]);
+      setCurrentMessage('');
+    }
+  };
   return <div></div>;
 }
 export default Chat;
