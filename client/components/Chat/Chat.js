6eb1af4eee0624cceb66ca32b5ba04609b98c13f
Sulaiman Marey <adammarey51@gmail.com>
2022-03-28 09:37:18 -0700

    built chat conponets

---
+++ client/components/Chat/Chat.js
@@ -0,0 +1,9 @@
+import React, { useEffect, useState } from 'react';
+
+function Chat({ socket, username, room }) {
+  const [currentMessage, setCurrentMessage] = useState('');
+  const [messageList, setMessageList] = useState([]);
+
+  return <div></div>;
+}
+export default Chat;
