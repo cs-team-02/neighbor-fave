8e3d28a324198ef022d5c5c4aa0527fc1b180e99
Konstantin Balakirev <kbalakirev@gmail.com>
2022-04-03 16:42:03 -0500

    removed top bar on signup and signin

--- client/components/TopMenu.js
+++ client/components/TopMenu.js
@@ -1,41 +1,36 @@
-import React from "react";
-import { connect } from "react-redux";
-import { Link } from "react-router-dom";
-import { logout } from "../store";
-import useAuth from "./utils/useAuthHook";
-import { CgLogOut } from "react-icons/cg";
-import { MdOutlineArrowBackIosNew } from "react-icons/md";
+import React from 'react';
+import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
+import { logout } from '../store';
+import useAuth from './utils/useAuthHook';
+import { CgLogOut } from 'react-icons/cg';
+import { MdOutlineArrowBackIosNew } from 'react-icons/md';
 
 const TopMenu = ({ isLoggedIn }) => {
   const currentUser = useAuth();
   return (
-    <div className="menu-bar">
-      <nav className="padding-div">
-        {isLoggedIn ? (
-          <div id="top-menu-div">
+    <div className='menu-bar'>
+      {/* <nav className='padding-div'> */}
+      {isLoggedIn ? (
+        <div className='padding-div'>
+          <div id='top-menu-div'>
             {/* The navbar will show these links after you log in */}
-
 
             <MdOutlineArrowBackIosNew onClick={() => history.back()} />
 
-            
             <div id='profile-thumb-div'>
               <img className='thumb-img' src={currentUser.ImageURL} />
-
 
               <Link to={`/profile`}>
                 <b>{currentUser.name}</b>
               </Link>
             </div>
           </div>
-        ) : (
-          <div className="right-text-div">
-            {/* The navbar will show these links before you log in */}
-            <Link to="/login">Login</Link>
-            <Link to="/signup">Sign Up</Link>
-          </div>
-        )}
-      </nav>
+        </div>
+      ) : (
+        <></>
+      )}
+      {/* </nav> */}
     </div>
   );
 };
