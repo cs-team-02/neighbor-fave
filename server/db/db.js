40fbf0a2b13277effc95fc03819d36180a1fd512
Mentor Kwame <kwame.gumbs@gmail.com>
2022-11-14 10:42:47 -0500

    add require for dotenv

--- server/db/db.js
+++ server/db/db.js
@@ -1,5 +1,6 @@
 const Sequelize = require('sequelize')
 const pkg = require('../../package.json')
+require("dotenv").config();
 
 const databaseName = pkg.name + (process.env.NODE_ENV === 'test' ? '-test' : '')
 
@@ -7,12 +8,12 @@ const config = {
   logging: false
 };
 
-if(process.env.LOGGING === 'true'){
+if (process.env.LOGGING === 'true') {
   delete config.logging
 }
 
 //https://stackoverflow.com/questions/61254851/heroku-postgres-sequelize-no-pg-hba-conf-entry-for-host
-if(process.env.DATABASE_URL){
+if (process.env.DATABASE_URL) {
   config.dialectOptions = {
     ssl: {
       rejectUnauthorized: false
